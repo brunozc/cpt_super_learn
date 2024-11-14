d7443f17b89e3ba697717f09c0ad60a4b6c86733
brunozc <brunozuada@gmail.com>
2024-11-14 13:14:31 +0100

    feat: add moving average function and integrate it into score plotting

--- CPTSuperLearn/utils.py
+++ CPTSuperLearn/utils.py
@@ -33,6 +33,10 @@ def input_random_data_file(training_data_folder: str) -> Tuple[str, np.ndarray]:
     -----------
     :param training_data_folder: folder with the training data
     :return: file name and data
+
+    Returns:
+    --------
+    :return: file name and data
     """
 
     # randomly initialise the field
@@ -44,6 +48,7 @@ def input_random_data_file(training_data_folder: str) -> Tuple[str, np.ndarray]:
     file_id, data = read_data_file(os.path.join(training_data_folder, files[idx]))
 
     return file_id, data
+
 
 def write_score(episode: List, total_score: List, output_file: str):
     """
@@ -61,3 +66,30 @@ def write_score(episode: List, total_score: List, output_file: str):
     with open(output_file, "w") as fo:
         for ep, score in zip(episode, total_score):
             fo.write(f"{ep};{score}\n")
+
+
+def moving_average(data: np.ndarray, window_size: int) -> np.ndarray:
+    """
+    Calculate the moving average
+
+    Parameters:
+    -----------
+    :param data: data
+    :param window_size: window size
+
+    Returns:
+    --------
+    :return: moving average
+    """
+
+    if data.ndim != 1:
+        raise(ValueError, "smooth only accepts 1 dimension arrays.")
+
+    if data.size < window_size:
+        raise(ValueError, "Input vector needs to be bigger than window size.")
+
+    s=np.r_[2*data[0] - data[window_size:1:-1], data, 2 * data[-1]-data[-1:-window_size:-1]]
+    w = np.ones(window_size, 'd')
+    y = np.convolve(w/w.sum(), s, mode='same')
+
+    return y[window_size-1:-window_size+1]
--- plot_score.py
+++ plot_score.py
@@ -1,13 +1,18 @@
+import numpy as np
 import matplotlib.pyplot as plt
+
+from CPTSuperLearn.utils import moving_average
+
 
 with open('./results/cpt_score.txt', 'r') as f:
     lines = f.read().splitlines()
 
-data = [line.split(";") for line in lines[1:]]
+data = np.array([line.split(";") for line in lines[1:]]).astype(float)
+
 
 fig, ax = plt.subplots()
-ax.plot([int(x[0]) for x in data], [float(x[1]) for x in data])
+ax.plot(data[:, 0], data[:, 1], color='b')
+ax.plot(data[:, 0], moving_average(data[:, 1], 10), color='r')
 ax.set(xlabel='Episode', ylabel='Score')
 ax.grid()
 plt.show()
-
