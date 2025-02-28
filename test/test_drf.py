23c2d9f7dcb33ee90d08971100745427e9639a9c
brunozc <brunozuada@gmail.com>
2025-02-28 17:16:08 +0100

    Add pytest import and skip decorator for WIP SchemaGAN model test

--- test/test_drf.py
+++ test/test_drf.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import random
 import shutil
+import pytest
 import numpy as np
 import torch
 import tensorflow as tf
@@ -101,6 +102,7 @@ def test_inv_dist():
 
     shutil.rmtree(output_folder)
 
+@pytest.mark.skip(reason="SchemaGAN model test is WIP")
 def test_inv_schemaGAN():
     """
     Test the model with schemaGAN interpolation
