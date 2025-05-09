08bd591e097bd2ddc8801f14d09d65673ffe2d4c
brunozc <brunozuada@gmail.com>
2025-05-09 13:50:45 +0200

    Add configuration file and refactor training script to load settings from YAML

--- train_model.py
+++ train_model.py
@@ -2,6 +2,8 @@ import os
 import random
 import numpy as np
 import torch
+import yaml
+import argparse
 
 from CPTSuperLearn.utils import input_random_data_file
 from CPTSuperLearn.environment import CPTEnvironment
@@ -9,14 +11,79 @@ from CPTSuperLearn.agent import DQLAgent
 from CPTSuperLearn.interpolator import InverseDistance, SchemaGANInterpolator
 from CPTSuperLearn.metric import MetricsTracker
 
-# fix all the seeds
-random.seed(14)
-np.random.seed(14)
-torch.manual_seed(14)
+
+def load_config(config_path):
+    """
+    Load configuration from YAML file
+
+    Parameters
+    ----------
+    :param config_path: Path to the configuration file
+    :return: Configuration dictionary
+    """
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
 
 
-def main(nb_episodes: int, cpt_env: CPTEnvironment, agent: DQLAgent, training_data_folder: str, output_folder: str,
-         make_plots=False):
+def main(config_path: str):
+    """
+    Main function to train the CPTSuperLearn model.
+
+    Parameters
+    ----------
+    :param config_path: Path to the yml configuration file.
+    """
+
+    # Load configuration
+    config = load_config(config_path)
+
+    # fix all the seeds
+    random.seed(config['seed'])
+    np.random.seed(config['seed'])
+    torch.manual_seed(config['seed'])
+
+    data_config = config['data']
+    agent_config = config['agent']
+    environment_config = config['environment']
+    training_config = config['training']
+
+    # Set up agent
+    cpt_agent = DQLAgent(
+        state_size=agent_config['state_size'],
+        action_size=len(environment_config['actions']),
+        learning_rate=agent_config['learning_rate'],
+        gamma=agent_config['gamma'],
+        epsilon_start=agent_config['epsilon_start'],
+        epsilon_end=agent_config['epsilon_end'],
+        epsilon_decay=agent_config['epsilon_decay'],
+        memory_size=agent_config['memory_size'],
+        batch_size=agent_config['batch_size'],
+        nb_steps_update=agent_config['nb_steps_update'],
+        hidden_layers=agent_config['hidden_layers'],
+        use_batch_norm=agent_config['use_batch_norm'],
+        activation=agent_config['activation']
+    )
+
+    # Set up interpolation method
+    if environment_config['interpolation_method'] == 'SchemaGAN':
+        interpolator = SchemaGANInterpolator(model_path=environment_config['interpolation_method_params'])
+    elif environment_config['interpolation_method'] == 'InverseDistance':
+        interpolator = InverseDistance(nb_points=int(environment_config['interpolation_method_params']))
+
+    # Set up environment
+    cpt_env = CPTEnvironment(
+        action_list=environment_config['actions'],
+        max_nb_cpts=environment_config['max_nb_cpts'],
+        weight_reward_cpt=environment_config['weight_reward_cpt'],
+        image_width=environment_config['image_width'],
+        max_first_step=environment_config['max_first_step'],
+        interpolation_method=interpolator,
+    )
+
+    train(data_config, training_config, cpt_env, cpt_agent)
+
+def train(config_data: dict, config_training: dict, cpt_env: dict, agent: dict):
     """
     Train the DRL model
 
@@ -29,6 +96,10 @@ def main(nb_episodes: int, cpt_env: CPTEnvironment, agent: DQLAgent, training_da
     :param output_folder: output folder
     :param make_plots: make plots
     """
+
+    output_folder = config_training['output_folder']
+    nb_episodes = config_training['nb_episodes']
+    training_data_folder = config_data['train_folder']
 
     total_score = []
     metrics = MetricsTracker(output_folder)
@@ -54,10 +125,10 @@ def main(nb_episodes: int, cpt_env: CPTEnvironment, agent: DQLAgent, training_da
         average_score = np.mean(total_score)
         metrics.update(episode, score, agent, cpt_env, action_index)
 
-        if make_plots:
+        if config_training["make_plots"]:
             cpt_env.plot_environment(os.path.join(output_folder, "training_2", f"episode_{episode}_file_{file_name}"))
 
-        if episode % 10 == 0:
+        if episode % config_training['log_interval'] == 0:
             print(f"Episode {episode} / {nb_episodes} | Average score: {average_score:.2f} Epsilon: {agent.epsilon:.2f}")
 
     # save the model, agent, environment and score
@@ -66,38 +137,9 @@ def main(nb_episodes: int, cpt_env: CPTEnvironment, agent: DQLAgent, training_da
     metrics.visualize_metrics()
     metrics.save_metrics()
 
-
 if __name__ == "__main__":
-    training_data_folder = "data/train"
-    num_episodes = 10000
-    actions = [25, 50, 100, 150]  # More evenly spaced actions
-
-    # Experiment with the improved reward function
-    output_folder = "results2"
-
-    cpt_env = CPTEnvironment(
-        actions,
-        max_nb_cpts=20,
-        weight_reward_cpt=0.6,
-        image_width=512,
-        max_first_step=1,
-        interpolation_method=SchemaGANInterpolator(model_path="./schemaGAN/schemaGAN.h5"),
-    )
-
-    cpt_agent = DQLAgent(
-        state_size=6,
-        action_size=len(actions),
-        learning_rate=1e-4,
-        gamma=0.99,
-        epsilon_start=0.95,
-        epsilon_end=0.05,
-        epsilon_decay=0.9996,
-        memory_size=10000,
-        batch_size=64,
-        nb_steps_update=100,
-        hidden_layers=[32, 64, 64, 32],
-        use_batch_norm=True,
-        activation='relu'
-    )
-
-    main(num_episodes, cpt_env, cpt_agent, training_data_folder, output_folder, make_plots=False)
+    parser = argparse.ArgumentParser(description="Train the CPTSuperLearn model.")
+    parser.add_argument("--config", type=str, required=True,
+                        help="Path to the configuration file (required)")
+    args = parser.parse_args()
+    main(args.config)
